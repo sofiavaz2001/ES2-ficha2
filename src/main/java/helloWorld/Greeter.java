cedc4875d870c2ebb9c5597454d0b45d35a157ba
sofiavaz2001 <sofiavaz2001@hotmail.com>
2024-09-30 23:08:18 +0100

    no branch

---
+++ src/main/java/helloWorld/Greeter.java
@@ -0,0 +1,7 @@
+package helloWorld;
+public class Greeter {
+	public String sayHello() {
+		return "Hello world!";
+	}
+}
+
---
+++ src/main/java/helloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+package helloWorld;
+public class HelloWorld {
+	public static void main(String[] args) {
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+	}
+}
